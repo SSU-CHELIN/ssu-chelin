7dd832d4a72f377c3d1e980ce12019d756d18c02
s-hwan <tmdghks507@daum.net>
2024-11-19 13:13:19 +0900

    menu fragment 띄우는건 성공

--- app/src/main/java/com/example/ssuchelin/MainActivity.java
+++ app/src/main/java/com/example/ssuchelin/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ssuchelin;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -13,51 +14,75 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        if (savedInstanceState == null) {
+//            String openFragment = getIntent().getStringExtra("open_fragment");
+//
+//            if ("MenuFragment".equals(openFragment)) {
+//                switchFragment(new MenuFragment());
+//            } else {
+//                switchFragment(new MenuFragment()); // 기본 프래그먼트도 MainViewFragment로 설정
+//            }
+//        }
+//
+//        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+//        bottomNavigationView.setOnItemSelectedListener(item -> {
+//            if (item.getItemId() == R.id.page_1) {
+//                switchFragment(new MenuFragment());
+//                return true;
+//            } else if (item.getItemId() == R.id.page_2) {
+//                switchFragment(new ReviewFragment());
+//                return true;
+//            } else if (item.getItemId() == R.id.page_3) {
+//                switchFragment(new RankingFragment());
+//                return true;
+//            } else if (item.getItemId() == R.id.page_4) {
+//                switchFragment(new ProfileViewFragment());
+//                return true;
+//            }
+//            return false;
+//        });
+//    }
+//
+//    public void switchFragment(Fragment fragment) {
+//        String fragmentTag = fragment.getClass().getSimpleName();
+//        Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+//
+//        if (currentFragment != null && currentFragment.getClass().getSimpleName().equals(fragmentTag)) {
+//            return; // 이미 표시 중인 프래그먼트라면 전환하지 않음
+//        }
+//
+//        getSupportFragmentManager().beginTransaction()
+//                .replace(R.id.fragment_container, fragment, fragmentTag)
+//                .addToBackStack(null)
+//                .commit();
+//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            String openFragment = getIntent().getStringExtra("open_fragment");
-
-            if ("MenuFragment".equals(openFragment)) {
-                switchFragment(new MenuFragment());
-            } else {
-                switchFragment(new MenuFragment()); // 기본 프래그먼트도 MainViewFragment로 설정
+        // Intent에서 "open_fragment" 값을 가져옴
+        String fragmentToOpen = getIntent().getStringExtra("open_fragment");
+        if (fragmentToOpen != null) {
+            switch (fragmentToOpen) {
+                case "MenuFragment":
+                    openFragment(new MenuFragment());
+                    break;
+                // 필요한 경우 다른 프래그먼트를 처리
             }
         }
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            if (item.getItemId() == R.id.page_1) {
-                switchFragment(new MenuFragment());
-                return true;
-            } else if (item.getItemId() == R.id.page_2) {
-                switchFragment(new ReviewFragment());
-                return true;
-            } else if (item.getItemId() == R.id.page_3) {
-                switchFragment(new RankingFragment());
-                return true;
-            } else if (item.getItemId() == R.id.page_4) {
-                switchFragment(new ProfileViewFragment());
-                return true;
-            }
-            return false;
-        });
     }
 
-    public void switchFragment(Fragment fragment) {
-        String fragmentTag = fragment.getClass().getSimpleName();
-        Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
-
-        if (currentFragment != null && currentFragment.getClass().getSimpleName().equals(fragmentTag)) {
-            return; // 이미 표시 중인 프래그먼트라면 전환하지 않음
-        }
-
+    private void openFragment(@NonNull Fragment fragment) {
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, fragment, fragmentTag)
-                .addToBackStack(null)
+                .replace(R.id.fragment_container, fragment)
                 .commit();
     }
+
+
 }
